b0c81a2758d6d06c3058b62e1c533123f7c77dce
vkbhandare <vkbhandare00000@users.noreply.github.com>
2023-03-24 10:39:08 +0530

    df

--- JS/custom.js
+++ JS/custom.js
@@ -1,2 +1,9 @@
 
+document.querySelector('.hamburger').
+addEventListener('click', () =>
+    document.querySelector('nav').classList.toggle('show'));
 
+
+window.addEventListener('scroll', () => {
+document.querySelector('header').classList.toggle('changeColor', window.scrollY > 10)
+})
